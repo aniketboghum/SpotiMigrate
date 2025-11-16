d6cd29db58cbb7172580878908039e0dabb17e71
aniketboghum <146850713+aniketboghum@users.noreply.github.com>
2025-11-16 18:04:19 +0530

    changes

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -6,14 +6,12 @@ export default function Header() {
         <header className="fixed top-0 inset-x-0 z-50 border-b bg-background/70 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="mx-auto max-w-6xl px-4 sm:px-6 h-16 flex items-center justify-between">
             <div className="flex items-center gap-3">
-            <a href="/">
                 <img
                     src="/icons/spotimigrate_name.png"
                     alt="SpotiMigrate logo"
                     className="h-8 w-auto sm:h-9 md:h-10 rounded md:rounded-md"
                     
                 />
-            </a>
             <span className="hidden sm:inline text-lg font-logo font-extrabold">SpotiMigrate</span>
             </div>
                 <div className="hidden sm:flex items-center gap-3">
